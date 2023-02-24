f8f58f60de7e16f3a35ea07d6b0fc3fc8df417be
Muhammet Mustafa Dincer <121862751+mmdincer@users.noreply.github.com>
2023-02-24 21:08:13 +0300

    Update dosya_kopyalama.sh

--- dosya_kopyalama.sh
+++ dosya_kopyalama.sh
@@ -1,9 +1,47 @@
-src_dir= # Kaynak klasör yolu
+#!/bin/bash
 
-dst_dir= # Hedef klasör yolu
+# Kullanıcıdan kaynak dosya yolunu alın
+echo "Lütfen kaynak dosyanın yolunu girin: "
+read kaynak
 
-# Dosyaları kopyala
-cp -r $src_dir/* $dst_dir
+# Kaynak dosyanın mevcut olup olmadığını kontrol edin
+if [ ! -f "$kaynak" ]; then
+  echo "Hata: $kaynak bulunamadı!"
+  exit 1
+fi
 
-# Başarı mesajı göster
-echo "Dosyalar kopyalandi"
+# Kullanıcıdan hedef dizin yolunu alın
+echo "Lütfen hedef dizinin yolunu girin: "
+read hedef
+
+# Hedef dizinin mevcut olup olmadığını kontrol edin
+if [ ! -d "$hedef" ]; then
+  echo "Hata: $hedef bulunamadı!"
+  exit 1
+fi
+
+# Kullanıcıdan seçenekleri alın
+echo "Lütfen seçeneği girin: kopyala (k), taşı (t) veya sil (s)"
+read secenek
+
+# Seçeneklere göre işlem yapın
+case $secenek in
+  k|K)
+       cp "$kaynak" "$hedef"
+       echo "Dosya başarıyla kopyalandı!"
+       ;;
+  t|T)
+       mv "$kaynak" "$hedef"
+       echo "Dosya başarıyla taşındı!"
+       ;;
+  s|S)
+       rm "$kaynak"
+       echo "Dosya başarıyla silindi!"
+       ;;
+  *) 
+     echo "Hata: yanlış seçenek girildi!"
+     exit 1
+     ;;
+esac
+
+exit 0
